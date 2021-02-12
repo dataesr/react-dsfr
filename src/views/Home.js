75dcb29f796fc111a88a3fa38cd8e7c1e93d6707
folland87 <frederic@olland.me>
2021-02-12 09:34:09 +0100

    add TextInput component

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import { useState } from 'react';
 import Button from '../components/Button';
+import TextInput from '../components/TextInput';
 
 const Home = () => {
+  const [value, setValue] = useState('')
   return (
+    <>
     <section>
       <Button text="Bouton du Gouvernement"/>
     </section>
+    <section>
+      <TextInput
+        value={value}
+        onChange={e => setValue(e.target.value)}
+        label="Text input du gouvernement"
+        message="Y'a une erreur non ?"
+        messageType="error"
+        hint="Je suis un champs de saisie de texte."
+      />
+    </section>
+    </>
   );
 };
 
