1fc7866b2f802fee1a125c9250a586fc433f61e9
jerem1508 <jeremy.peglion@gmail.com>
2021-02-19 21:35:49 +0100

    init Select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,36 +1,100 @@
 import { forwardRef } from 'react';
 import PropTypes from 'prop-types';
-import '@gouvfr/all/dist/css/all.min.css';
+import { v4 as uuidv4 } from 'uuid';
+import classNames from 'classnames';
+// import '@gouvfr/selects/dist/css/selects.min.css';
 
 /**
- * La liste déroulante permet à un utilisateur de choisir un élément dans une liste donnée. 
+ * La liste déroulante permet à un utilisateur de choisir un élément dans une liste donnée.
  *
  * @visibleName Liste déroulante - Select
  */
-const Select = forwardRef((props, ref) => {
-  const {
+const Select = ({
+  className,
+  disabled,
+  hint,
+  id,
+  label,
+  message,
+  messageType,
+  name,
+  onChange,
+  options,
+}) => {
+  const selectClasses = classNames(
+    'rf-select',
     className,
-    id,
-    name,
-    children,
-  } = props;
+    `${(messageType ? `rf-select--${messageType}` : '')}`,
+  );
+  const selectId = id || uuidv4();
+  const messageId = uuidv4();
+  const divClasses = classNames(`${(messageType ? `rf-select-group rf-select-group--${messageType}` : '')}`);
 
   return (
-    <select className={`rf-select ${className || ''}`} id={id || null} name={name || null}>
-      {children}
-    </select>
+    <div className={divClasses}>
+      {
+      label && (
+        <label className="rf-label" htmlFor={selectId} aria-describedby={messageId}>
+          {label}
+          {hint && <span className="rf-hint-text" id={`${selectId}-desc-hint`}>{hint}</span>}
+        </label>
+      )}
+      <select
+        className={selectClasses}
+        data-testid="select"
+        disabled={disabled}
+        id={selectId}
+        name={name}
+        onChange={onChange}
+      >
+        {
+          options.map((opt) => (
+            <option
+              disabled={opt.disabled || false}
+              hidden={opt.hidden || false}
+              key={uuidv4()}
+              selected={opt.selected || false}
+              value={opt.value}
+            >
+              {opt.label}
+            </option>
+          ))
+        }
+      </select>
+      {(message && messageType) && <p id={messageId} className={`rf-${messageType}-text`}>{message}</p>}
+    </div>
   );
-});
+};
 
-// Select.defaultProps = {
-//   textarea: false,
-//   inactive: false,
-// }
+Select.defaultProps = {
+  className: '',
+  disabled: false,
+  hint: '',
+  id: null,
+  label: '',
+  message: '',
+  messageType: '',
+  name: null,
+  onChange: () => {},
+};
+
 Select.propTypes = {
   className: PropTypes.string,
+  disabled: PropTypes.bool,
+  hint: PropTypes.string,
   id: PropTypes.string,
+  label: PropTypes.string,
+  message: PropTypes.string,
+  messageType: PropTypes.oneOf(['error', 'valid']),
   name: PropTypes.string,
-  children: PropTypes.array,
+  onChange: PropTypes.func,
+  options: PropTypes.arrayOf(PropTypes.shape({
+    value: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    selected: PropTypes.bool,
+    disabled: PropTypes.bool,
+    hidden: PropTypes.bool,
+  })).isRequired,
 };
 
 export default Select;
---
+++ src/components/Select/__tests__/Select.test.js
@@ -0,0 +1,47 @@
+import { render, screen } from '@testing-library/react';
+import Select from '..';
+
+describe('<Select />', () => {
+  const options = [
+    { value: '1', label: 'label-1' },
+    { value: '2', label: 'label-2' },
+  ];
+
+  it('renders Select properly', () => {
+    // const onChange = jest.fn();
+    render(
+      <Select
+        label="Label pour liste déroulante"
+        options={options}
+      />,
+    );
+    const search = screen.getByTestId('select');
+    expect(search).toBeInTheDocument();
+    expect(search).toMatchSnapshot();
+  });
+
+  // test du nombre d'items
+
+  // test de la présence de la class rf-select
+
+  // test du label
+
+  // test de la fonction onChange
+
+  // test de la class d'erreur
+  // label
+  // select
+  // message
+  // aria
+
+  // test de la class succès
+  // label
+  // select
+  // message
+  // aria
+
+  // test disable
+
+  // test hint
+  // ...
+});
