a29bb6080227b360c1be4ec1ce40446470f7df21
folland87 <frederic@olland.me>
2021-02-19 09:54:31 +0100

    accordion alternative API

---
+++ src/components/Accordion2/Accordion.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import classNames from 'classnames';
+import { CHILDREN_TYPE, CLASS_NAME_TYPE } from '../../utils/variables';
+
+/**
+ * Condenser l'espace
+ *
+ * @visibleName Accordion
+ */
+const AccordionGroup = ({ className, children }) => {
+  const [isExpanded, setIsExpanded] = useState('');
+  const expand = (e) => {
+    if (e.target.id.slice(6) === isExpanded) {
+      setIsExpanded('');
+    } else {
+      setIsExpanded(e.target.id.slice(6));
+    }
+  };
+
+  const childs = React.Children.toArray(children).map((child, i) => {
+    const id = i.toString();
+    return React.cloneElement(child, {
+      id,
+      key: id,
+      onClick: expand,
+      isExpanded: (isExpanded === id),
+    });
+  });
+
+  return (
+    <section className={classNames(className)}>
+      <ul className="rf-accordions-group">
+        {childs}
+      </ul>
+    </section>
+  );
+};
+
+AccordionGroup.defaultProps = {
+  children: '',
+  className: '',
+};
+
+AccordionGroup.propTypes = {
+  children: CHILDREN_TYPE,
+  className: CLASS_NAME_TYPE,
+};
+
+export default AccordionGroup;
