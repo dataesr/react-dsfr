26d2fef36881ff1b43c9814ee918b64045506678
jerem1508 <jeremy.peglion@gmail.com>
2021-03-11 10:34:07 +0100

    fix id for snapshot

--- src/components/interface/Checkbox/__tests__/Checkbox.test.js
+++ src/components/interface/Checkbox/__tests__/Checkbox.test.js
@@ -1,7 +1,28 @@
 import { render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+import { v4 as uuidv4 } from 'uuid';
 import { Checkbox } from '../index';
 
+jest.mock('uuid', () => ({
+  v4: jest.fn(),
+}));
+
 describe('<Checkbox />', () => {
+  beforeEach(() => {
+    uuidv4.mockImplementationOnce(() => 'xxxxxxx');
+  });
+  it('renders correctly', () => {
+    const component = renderer
+      .create(
+        <Checkbox
+          label="Label de la checkbox"
+          value="value"
+        />,
+      )
+      .toJSON();
+    expect(component).toMatchSnapshot();
+  });
+
   it('in document', () => {
     render(
       <Checkbox
