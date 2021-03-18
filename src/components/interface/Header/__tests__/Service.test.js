e15004424c9073eee3893dd19f7f8de5ea729079
PaulineGC <francasix@outlook.com>
2021-03-18 10:57:40 +0100

    Feature/header + navigation (#47)
    Header

---
+++ src/components/interface/Header/__tests__/Service.test.js
@@ -0,0 +1,43 @@
+import renderer from 'react-test-renderer';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { render } from '@testing-library/react';
+import {
+  Service,
+} from '../index';
+import HeaderContext from '../headerContext';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('<Service />', () => {
+  const context = {
+    onOpenNav: jest.fn(),
+    isMobile: true,
+    isNavTool: true,
+    navButton: 'navigation',
+  };
+  it('renders correctly', () => {
+    const component = renderer
+      .create(
+        <Service
+          title="title"
+          description="description service"
+        />,
+      )
+      .toJSON();
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should call onOpenNav on click', () => {
+    const { getByText } = render(
+      <HeaderContext.Provider value={context}>
+        <Service
+          title="title"
+          description="description service"
+        />
+        ,
+      </HeaderContext.Provider>,
+    );
+    expect(getByText(/navigation/i).textContent).toBe('navigation');
+  });
+});
