dadf1cf1735ed4c670a41fabebee8526607aa991
paulinegc <francasix@outlook.com>
2021-04-19 12:10:57 +0200

    autoreview

--- src/components/interface/Button/Button.js
+++ src/components/interface/Button/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-// import '../../../style.css';
+import '../../../style.css';
 import './schemes.css';
 import './buttons.css';
 
--- src/components/interface/SearchBar/SearchBar.js
+++ src/components/interface/SearchBar/SearchBar.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { v4 as uuidv4 } from 'uuid';
 import classNames from 'classnames';
 
+import '../../../style.css';
 import './search.css';
 
 /**
