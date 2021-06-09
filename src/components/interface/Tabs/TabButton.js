912a646eda87244b72c1c4285b8c840ddfb61341
Xavierd <xavier.desoindre@hotmail.fr>
2021-06-09 14:08:03 +0200

    Fix active tab

--- src/components/interface/Tabs/TabButton.js
+++ src/components/interface/Tabs/TabButton.js
@@ -1,49 +1,36 @@
 import React from 'react';
-import classNames from 'classnames';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import dataAttributes from '../../../utils/data-attributes';
-import Icon from '../../foundation/icon/index';
 
 const TabButton = ({
-  className, index, activeTab, icon, iconPosition, label, setActiveTab, ...remainingProps
-}) => {
-  const _tab = (
-    <li
+  className, index, activeTab, icon, label, setActiveTab, ...remainingProps
+}) => (
+  <li
+    role="presentation"
+    {...dataAttributes(remainingProps)}
+  >
+    <button
+      type="button"
       role="tab"
       aria-selected={activeTab === index ? 'true' : 'false'}
-      className={classNames(className)}
       tabIndex="0"
       aria-controls={`fr-tabpanel-${index}`}
+      className={classNames('fr-tabs__tab', {
+        [icon]: icon,
+        'fr-tabs__tab--icon-left': icon,
+      })}
       onClick={() => setActiveTab(index)}
       onKeyDown={() => setActiveTab(index)}
     >
-      <div
-        className={classNames('fr-tabs__tab', {
-          [`fr-tabs__tab--icon-${iconPosition}`]: icon && iconPosition,
-          [`icon-${iconPosition}`]: icon && iconPosition,
-        })}
-      >
-        {label}
-      </div>
-    </li>
-  );
-  return (
-    <div
-      {...dataAttributes(remainingProps)}
-    >
-      {icon ? (
-        <Icon name={icon} size="lg" iconPosition={icon && 'left'}>
-          {_tab}
-        </Icon>
-      ) : _tab}
-    </div>
-  );
-};
+      {label}
+    </button>
+  </li>
+);
 
 TabButton.defaultProps = {
   className: '',
   icon: '',
-  iconPosition: 'left',
 };
 
 TabButton.propTypes = {
@@ -55,7 +42,6 @@ TabButton.propTypes = {
   setActiveTab: PropTypes.func.isRequired,
   label: PropTypes.string.isRequired,
   icon: PropTypes.string,
-  iconPosition: PropTypes.oneOf(['left', 'right']),
   index: PropTypes.number.isRequired,
   activeTab: PropTypes.number.isRequired,
 };
