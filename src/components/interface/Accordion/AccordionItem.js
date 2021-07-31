5a1363ced00b4015ed2a5900815164969ba0f0d0
paulinegc <paulinegc@protonmail.com>
2021-07-31 17:27:16 +0200

    forwardRef AccordionItem

--- src/components/interface/Accordion/AccordionItem.js
+++ src/components/interface/Accordion/AccordionItem.js
@@ -1,4 +1,5 @@
 import React, {
+  forwardRef,
   useCallback, useEffect, useRef, useState,
 } from 'react';
 import classNames from 'classnames';
@@ -6,20 +7,21 @@ import PropTypes from 'prop-types';
 import useCollapse from '../../../hooks/useCollapse';
 import dataAttributes from '../../../utils/data-attributes';
 
-const AccordionItem = ({
-  title,
-  titleAs,
-  expandedItems,
-  onClick,
-  children,
-  className,
-  id,
-  keepOpen,
-  initExpand,
-  ...remainingProps
-}) => {
+const AccordionItem = forwardRef((props, ref) => {
+  const {
+    title,
+    titleAs,
+    expandedItems,
+    onClick,
+    children,
+    className,
+    id,
+    keepOpen,
+    initExpand,
+    ...remainingProps
+  } = props;
   const TitleTag = `${titleAs}`;
-  const ref = useRef();
+  const buttonRef = useRef();
   const [initItem, setInitItem] = useState(initExpand);
   const checkExpanded = useCallback(() => {
     const idsExpanded = !!expandedItems.find((it) => it === id);
@@ -28,7 +30,7 @@ const AccordionItem = ({
   const [isExpanded, setIsExpanded] = useState(checkExpanded);
   const { item, collapse } = useCollapse(`fr-accordion-${id}`, isExpanded);
   const onItemClick = (e) => {
-    const trullyExpanded = (ref.current.ariaExpanded === 'true');
+    const trullyExpanded = (buttonRef.current.ariaExpanded === 'true');
     onClick(e, parseFloat(e.target.id.slice(6)), trullyExpanded);
     setInitItem(false);
   };
@@ -40,11 +42,12 @@ const AccordionItem = ({
     <li
       className={classNames(className)}
       {...dataAttributes(remainingProps)}
+      ref={ref}
     >
       <section className="fr-accordion">
         <TitleTag className="fr-accordion__title">
           <button
-            ref={ref}
+            ref={buttonRef}
             className="fr-accordion__btn"
             id={`button${id}`}
             onClick={(e) => onItemClick(e)}
@@ -67,7 +70,7 @@ const AccordionItem = ({
       </section>
     </li>
   );
-};
+});
 
 AccordionItem.defaultProps = {
   titleAs: 'h3',
@@ -75,26 +78,27 @@ AccordionItem.defaultProps = {
   initExpand: false,
   expandedItems: [],
   keepOpen: false,
-  onClick: () => {},
+  onClick: () => {
+  },
   id: 0,
 };
 
 AccordionItem.propTypes = {
   /**
-   * Html tag to render in accordion title.
-   */
+     * Html tag to render in accordion title.
+     */
   titleAs: PropTypes.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']),
   /**
-   * @ignore
-   */
+     * @ignore
+     */
   initExpand: PropTypes.bool,
   /**
-   * @ignore
-   */
+     * @ignore
+     */
   onClick: PropTypes.func,
   /**
-   * @ignore
-   */
+     * @ignore
+     */
   expandedItems: PropTypes.arrayOf(PropTypes.number),
   id: PropTypes.number,
   title: PropTypes.string.isRequired,
