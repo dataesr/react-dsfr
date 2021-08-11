61b27effc8d3a6582314fbf3bab993ae6d6ba565
Xavierd <xavier.desoindre@hotmail.fr>
2021-08-11 09:48:43 +0200

    Fix empty tag

--- example/src/components/Tag/Tag.jsx
+++ example/src/components/Tag/Tag.jsx
@@ -13,6 +13,7 @@ const TagExample = () => (
       <Tag size="sm" as="span">Label tag</Tag>
       <Tag size="sm" as="span" icon="ri-arrow-right-line" iconPosition="left">Label tag</Tag>
       <Tag size="sm" as="span" icon="ri-arrow-right-line">Label tag</Tag>
+      <Tag size="sm" as="span" icon="ri-check-line" />
     </TagGroup>
   </Col>
 );
