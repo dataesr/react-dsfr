7c19bcf99873cf87971545f4316fe52a2bae17de
paulinegc <paulinegc@protonmail.com>
2021-09-17 12:04:34 +0200

    callout

--- src/components/interface/Callout/Callout.js
+++ src/components/interface/Callout/Callout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import dataAttributes from '../../../utils/data-attributes';
@@ -10,13 +10,22 @@ import '@gouvfr/dsfr/dist/css/callouts.min.css';
  * @visibleName Callout
  */
 const Callout = ({
-  hasInfoIcon, children, className, ...remainingProps
+  hasInfoIcon, children, className, color, ...remainingProps
 }) => {
+  const calloutRef = useRef();
   const _className = classNames('fr-callout', className, {
     'fr-fi-information-line': hasInfoIcon,
   });
+
+  useEffect(() => {
+    if (color && calloutRef.current) {
+      calloutRef.current.style.boxShadow = `inset .25rem 0 0 0 ${color}`;
+    }
+  }, [color]);
+
   return (
     <div
+      ref={calloutRef}
       className={_className}
       {...dataAttributes.getAll(remainingProps)}
     >
@@ -24,8 +33,16 @@ const Callout = ({
     </div>
   );
 };
+
+Callout.defaultProps = {
+  hasInfoIcon: true,
+  className: '',
+  color: '',
+};
+
 Callout.propTypes = {
   hasInfoIcon: PropTypes.bool,
+  color: PropTypes.string,
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node,
@@ -35,10 +52,6 @@ Callout.propTypes = {
     PropTypes.object,
     PropTypes.array,
   ]),
-};
-Callout.defaultProps = {
-  hasInfoIcon: true,
-  className: '',
 };
 
 export default Callout;
