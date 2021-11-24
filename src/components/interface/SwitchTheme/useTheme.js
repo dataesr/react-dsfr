4d71b56a3e61e3a707bce1da3e3dc780c5d0c289
paulinegc <paulinegc@protonmail.com>
2021-11-24 13:29:52 +0100

    useTheme document prevent error

--- src/components/interface/SwitchTheme/useTheme.js
+++ src/components/interface/SwitchTheme/useTheme.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, useCallback } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 const useMutationObserver = (domNodeSelector, observerOptions, cb) => {
   useEffect(() => {
@@ -17,7 +17,7 @@ const useMutationObserver = (domNodeSelector, observerOptions, cb) => {
 const options = { attributes: true };
 
 const useTheme = () => {
-  const [theme, setTheme] = useState(document.documentElement.getAttribute('data-fr-theme'));
+  const [theme, setTheme] = useState();
 
   const handler = useCallback((mutationList) => {
     mutationList.forEach((mutation) => {
@@ -29,6 +29,11 @@ const useTheme = () => {
 
   useMutationObserver('html', options, handler);
 
+  useEffect(() => {
+    if (!theme) {
+      setTheme(document.documentElement.getAttribute('data-fr-theme'));
+    }
+  }, [theme]);
   return theme;
 };
 
