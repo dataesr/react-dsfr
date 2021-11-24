63535ff600b6428ffb7edde571f109fd45db168e
paulinegc <paulinegc@protonmail.com>
2021-11-24 16:54:17 +0100

    colorFamily fix pagination dark theme

--- example/src/components/Tag/Tag.jsx
+++ example/src/components/Tag/Tag.jsx
@@ -11,10 +11,10 @@ const TagExample = () => (
       <Tag size="sm" as="span" icon="ri-arrow-right-line" iconPosition="left">Label tag</Tag>
       <Tag size="sm" as="span" icon="ri-arrow-right-line">Label tag</Tag>
       <Tag size="sm" as="span">Label tag</Tag>
-      <Tag size="sm" as="span" icon="ri-arrow-right-line" iconPosition="left">Label tag</Tag>
       <Tag size="sm" as="span" icon="ri-arrow-right-line">Label tag</Tag>
       <Tag size="sm" as="span" icon="ri-check-line" />
     </TagGroup>
+    <Tag colorFamily="pink-tuile" icon="ri-arrow-right-line" iconPosition="left">Label tag with colorFamily</Tag>
   </Col>
 );
 
---
+++ src/utils/variables.js
@@ -0,0 +1,3 @@
+const colorFamilies = ['green-tilleul-verveine', 'green-bourgeon', 'green-emeraude', 'green-menthe', 'green-archipel', 'blue-ecume', 'blue-cumulus', 'purple-glycine', 'pink-macaron', 'pink-tuile', 'yellow-tournesol', 'yellow-moutarde', 'orange-terre-battue', 'brown-cafe-creme', 'brown-caramel', 'brown-opera', 'beige-gris-galet'];
+
+export default colorFamilies;
