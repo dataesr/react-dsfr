38552f1b643eedc3739e5ba892a6e1905a13b14c
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2022-02-04 18:58:15 +0100

    fix rollup lint

--- rollup.config.js
+++ rollup.config.js
@@ -1,64 +1,53 @@
-import { visualizer } from "rollup-plugin-visualizer";
-import resolve from "@rollup/plugin-node-resolve";
-import babel from "@rollup/plugin-babel";
-import postcss from "rollup-plugin-postcss";
-import image from "@rollup/plugin-image";
-import { terser } from "rollup-plugin-terser";
-import svgr from "@svgr/rollup";
-import dts from "rollup-plugin-dts";
-
-import pkg from "./package.json";
+import { visualizer } from 'rollup-plugin-visualizer';
+import resolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+import postcss from 'rollup-plugin-postcss';
+import image from '@rollup/plugin-image';
+import { terser } from 'rollup-plugin-terser';
+import svgr from '@svgr/rollup';
+import pkg from './package.json';
+import dts from 'rollup-plugin-dts';
 
 const MODE = [
   {
-    format: "cjs",
+    format: 'cjs',
   },
   {
-    format: "esm",
+    format: 'esm',
   },
 ];
 
 const config = [];
+
 MODE.forEach((m) => {
   const conf = {
     input: pkg.source,
     output: {
-      name: "@dataesr/react-dsfr",
+      name: '@dataesr/react-dsfr',
       file: `dist/index.min.${m.format}.js`,
       format: m.format,
-      exports: "auto",
+      exports: 'auto',
       globals: {
-        react: "React",
-        "react-dom": "ReactDOM",
-        "prop-types": "PropTypes",
-        classnames: "classNames",
-        uuid: "uuid",
-        "@babel/runtime/helpers/slicedToArray": "_slicedToArray",
-        "@babel/runtime/helpers/defineProperty": "_defineProperty",
-        "@babel/runtime/helpers/objectWithoutProperties":
-          "_objectWithoutProperties",
+        react: 'React',
+        'react-dom': 'ReactDOM',
+        'prop-types': 'PropTypes',
+        classnames: 'classNames',
+        uuid: 'uuid',
+        '@babel/runtime/helpers/slicedToArray': '_slicedToArray',
+        '@babel/runtime/helpers/defineProperty': '_defineProperty',
+        '@babel/runtime/helpers/objectWithoutProperties': '_objectWithoutProperties',
       },
     },
-    external: [
-      "uuid",
-      "classnames",
-      "prop-types",
-      "react",
-      "react-dom",
-      /@babel\/runtime/,
-    ],
+    external: ['uuid', 'classnames', 'prop-types', 'react', 'react-dom', /@babel\/runtime/],
     plugins: [
       terser(),
       babel({
-        exclude: "node_modules/**",
-        plugins: [
-          "@babel/transform-runtime",
-          "@babel/plugin-proposal-nullish-coalescing-operator",
-        ],
-        babelHelpers: "runtime",
+        exclude: 'node_modules/**',
+        plugins: ['@babel/transform-runtime', '@babel/plugin-proposal-nullish-coalescing-operator'],
+        babelHelpers: 'runtime',
       }),
       postcss({
-        extensions: [".css"],
+        extensions: ['.css'],
         minimize: true,
         plugins: [],
       }),
@@ -79,4 +68,6 @@ config.push({
   plugins: [dts()],
 });
 
-export default [...config];
+export default [
+  ...config,
+];
