245489527b40528027440ea057e216d573bfe24f
Maxime Golfier <25312957+maxgfr@users.noreply.github.com>
2022-03-07 08:49:52 +0100

    fix: missing types + build (#233)
    * fix: types
    
    * feat: add github action

--- src/components/interface/Tabs/Tab.d.ts
+++ src/components/interface/Tabs/Tab.d.ts
@@ -1,2 +1,17 @@
 import * as React from 'react';
 
+export type TabClassName = string | Object | any[];
+
+export type TabChildren = React.ReactNode[] | React.ReactNode | string;
+
+
+export interface TabProps {
+    className?: TabClassName;
+    children?: TabChildren;
+    index: number;
+    activeTab: number;
+}
+
+declare const Tab: React.FC<TabProps>;
+
+export default Tab;
