d59627c6ae64307932794389c3588d0b46abc6d4
Desoindx <xavier.desoindre@hotmail.fr>
2022-06-27 11:30:30 +0200

    fix: type of service

--- src/components/interface/Header/Service.d.ts
+++ src/components/interface/Header/Service.d.ts
@@ -4,8 +4,8 @@ export type ServiceClassName = string | Object | any[];
 
 export interface ServiceProps {
     __TYPE?: any;
-    description?: React.ReactElement<any>;
-    title: React.ReactElement<any>;
+    description?: React.ReactNode;
+    title: React.ReactNode;
     link?: string;
     className?: ServiceClassName;
     asLink?: React.ReactElement<any>;
