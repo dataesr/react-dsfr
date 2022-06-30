be070d2e2441cda07427a6332a27df564ccc93f9
paulinegc <paulinegc@protonmail.com>
2022-06-30 15:44:36 +0200

    review

--- src/components/interface/Stepper/Stepper.d.ts
+++ src/components/interface/Stepper/Stepper.d.ts
@@ -2,10 +2,14 @@ import * as React from 'react';
 
 export type StepperClassName = string | Object | any[];
 
+export type TitleAs = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+
+
 export interface StepperProps {
     steps: number;
     currentStep: number;
     className?: StepperClassName;
+    titleAs?: TitleAs;
 }
 
 declare const Stepper: React.FC<StepperProps>;
--- src/components/interface/Stepper/Stepper.js
+++ src/components/interface/Stepper/Stepper.js
@@ -6,16 +6,17 @@ import dataAttributes from '../../../utils/data-attributes';
 import '@gouvfr/dsfr/dist/component/stepper/stepper.css';
 
 const Stepper = ({
-  currentStep, steps, currentTitle, nextStepTitle, className, ...remainingProps
+  titleAs, currentStep, steps, currentTitle, nextStepTitle, className, ...remainingProps
 }) => {
   const _className = classNames('fr-stepper', className, {});
+  const HtmlTag = `${titleAs}`;
 
   return (
     <div className={_className} {...dataAttributes.getAll(remainingProps)}>
-      <h2 className="fr-stepper__title">
+      <HtmlTag className="fr-stepper__title">
         <span className="fr-stepper__state">{`Étape ${currentStep} sur ${steps}`}</span>
         {currentTitle}
-      </h2>
+      </HtmlTag>
       <div className="fr-stepper__steps" data-fr-current-step={currentStep} data-fr-steps={steps} />
       <p className="fr-stepper__details">
         <span className="fr-text--bold">Étape suivante : </span>
@@ -35,10 +36,12 @@ Stepper.propTypes = {
   steps: PropTypes.number.isRequired,
   currentTitle: PropTypes.string.isRequired,
   nextStepTitle: PropTypes.string.isRequired,
+  titleAs: PropTypes.oneOf(['h1', 'h2', 'h3', 'h4', 'h5']),
 };
 
 Stepper.defaultProps = {
   className: '',
+  titleAs: 'h4',
 };
 
 export default Stepper;
