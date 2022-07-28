0ef5aec447863ed9559d00ba5edc61e5e0933966
paulinegc <paulinegc@protonmail.com>
2022-07-28 17:30:32 +0200

    fix error classname

--- src/components/interface/Header/ToolTranslateItem.js
+++ src/components/interface/Header/ToolTranslateItem.js
@@ -6,16 +6,27 @@ import dataAttributes from '../../../utils/data-attributes';
 
 const ToolTranslateItem = ({
   className, children, hrefLang, href, onClick, active, ...remainingProps
-}) => (
-  <li
-    className={classNames(className)}
-    {...dataAttributes.getAll(remainingProps)}
-  >
-    <a className="fr-translate__language fr-nav__link" hrefLang={hrefLang} lang={hrefLang} href={href} aria-current={active || undefined} onClick={onClick}>
-      {children}
-    </a>
-  </li>
-);
+}) => {
+  const _className = classNames(className);
+
+  return (
+    <li
+      className={_className || undefined}
+      {...dataAttributes.getAll(remainingProps)}
+    >
+      <a
+        className="fr-translate__language fr-nav__link"
+        hrefLang={hrefLang}
+        lang={hrefLang}
+        href={href}
+        aria-current={active || undefined}
+        onClick={onClick}
+      >
+        {children}
+      </a>
+    </li>
+  );
+};
 
 ToolTranslateItem.defaultProps = {
   className: '',
