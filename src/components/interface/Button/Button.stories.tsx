3b4376b6d4d6a7972643fa783275d8333565ecf8
paulinegc <paulinegc@protonmail.com>
2022-08-01 16:52:17 +0200

    button start ok

--- src/components/interface/Button/Button.stories.tsx
+++ src/components/interface/Button/Button.stories.tsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { ComponentStory } from '@storybook/react';
 
-import Button  from './../components/interface/Button/Button';
+import Button from './Button';
 
 export default {
-  title: 'Example/Button',
+  title: 'Button',
   component: Button,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
     backgroundColor: { control: 'color' },
   },
-} as ComponentMeta<typeof Button>;
+};
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
@@ -18,7 +17,14 @@ const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
-  children: 'DSFR Button',
+  children: 'Button',
 };
 
-Primary.storyName = 'I am the primary';
+Primary.storyName = 'Primary';
+
+export const WithIcon = Template.bind({});
+
+WithIcon.args = {
+  children: 'Button',
+  icon: 'ri-alert-fill',
+};
