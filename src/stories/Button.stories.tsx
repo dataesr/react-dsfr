d88409c16aa40d1896ac0212b9bd8b9a5ac4273b
paulinegc <paulinegc@protonmail.com>
2022-08-01 11:01:51 +0200

    start

---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import Button  from './../components/interface/Button/Button';
+
+export default {
+  title: 'Example/Button',
+  component: Button,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof Button>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  children: 'DSFR Button',
+};
+
+Primary.storyName = 'I am the primary';
