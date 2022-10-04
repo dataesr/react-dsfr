e28c0bc4750290c61a067efad0a2d359a1166bdc
Eren Riviere <eren.riviere@finances.gouv.fr>
2022-10-04 13:25:54 +0200

    fix: current theme is not displayed in modal

--- src/components/interface/SwitchTheme/SwitchTheme.js
+++ src/components/interface/SwitchTheme/SwitchTheme.js
@@ -16,6 +16,7 @@ const SwitchTheme = ({
   isOpen, setIsOpen, title, legend, darkLabel, lightLabel, systemLabel,
 }) => {
   const currentTheme = useTheme();
+  const initialTheme = window.localStorage.getItem('prefers-color-scheme');
 
   const dark = window.matchMedia('(prefers-color-scheme: dark)').matches;
   const system = dark ? 'dark' : 'light';
@@ -23,18 +24,32 @@ const SwitchTheme = ({
   const themes = [
     { label: lightLabel, value: 'light', svg: <Light /> },
     { label: darkLabel, value: 'dark', svg: <Dark /> },
-    { label: systemLabel, value: system, svg: <System /> },
+    { label: systemLabel, value: 'system', svg: <System /> },
   ];
 
   useEffect(() => {
-    let initialTheme = window.localStorage.getItem('prefers-color-scheme');
+    let initTheme;
     if (!initialTheme) {
-      const preferedTheme = window.matchMedia('(prefers-color-scheme: dark)');
-      initialTheme = (preferedTheme && preferedTheme.matches) ? 'dark' : 'light';
+      const preferredTheme = window.matchMedia('(prefers-color-scheme: dark)');
+      initTheme = (preferredTheme && preferredTheme.matches) ? 'dark' : 'light';
+    } else {
+      switch (initialTheme) {
+        case lightLabel:
+          initTheme = 'light';
+          break;
+        case darkLabel:
+          initTheme = 'dark';
+          break;
+        case systemLabel:
+          initTheme = system;
+          break;
+        default:
+          initTheme = 'light';
+          break;
+      }
     }
-
-    document.documentElement.setAttribute('data-fr-theme', initialTheme);
-  }, []);
+    document.documentElement.setAttribute('data-fr-theme', initTheme);
+  }, [darkLabel, initialTheme, lightLabel, system, systemLabel]);
 
   return (
     <Modal
@@ -49,15 +64,22 @@ const SwitchTheme = ({
           legend={legend}
           value={currentTheme}
           onChange={(value) => {
-            window.localStorage.setItem('prefers-color-scheme', value);
-            document.documentElement.setAttribute('data-fr-theme', value);
+            if (value === systemLabel) {
+              window.localStorage.setItem('prefers-color-scheme', value);
+              document.documentElement.setAttribute('data-fr-theme', system);
+            } else {
+              const themeValue = value === darkLabel ? 'dark' : 'light';
+              window.localStorage.setItem('prefers-color-scheme', value);
+              document.documentElement.setAttribute('data-fr-theme', themeValue);
+            }
           }}
         >
           {themes.map((theme) => (
             <Radio
               key={theme.label}
               label={theme.label}
-              value={theme.value}
+              value={theme.label}
+              defaultChecked={theme.label === initialTheme}
               isExtended
               svg={theme.svg}
             />
