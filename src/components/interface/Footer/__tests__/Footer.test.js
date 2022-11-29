8ab6fba361246c2e2f119c632348a4a983e08b44
Sylvain LG <syl@sylvainlg.fr>
2022-11-29 18:23:44 +0100

    fix(Footer): :bug: Fix "filter" bug when FooterTopCategory has only one child

--- src/components/interface/Footer/__tests__/Footer.test.js
+++ src/components/interface/Footer/__tests__/Footer.test.js
@@ -98,4 +98,56 @@ describe('<Footer />', () => {
     const footer = screen.getByTestId('footer');
     expect(footer).toMatchSnapshot();
   });
+  it('should render Footer properly with only one child under FooterTopCategory', () => {
+    render(
+      <Footer data-testid="footer">
+        <FooterTop data-testid="footertop">
+          <FooterTopCategory title="Nom de la catégorie" data-testid="footertopcategory">
+            <FooterLink href="/" data-testid="footerlink">Lien de navigation</FooterLink>
+          </FooterTopCategory>
+        </FooterTop>
+        <FooterBody
+          description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Uenim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+          data-testid="footerbody"
+        >
+          <Logo splitCharacter={10} data-testid="footerlogo">
+            Ministère de l&apos;enseignement supérieur de la rechercher et de l&apos;innovation
+          </Logo>
+          <FooterOperator>
+            <img src="https://fakeimg.pl/145x81/" alt="texte alternatif" />
+          </FooterOperator>
+          <FooterBodyItem>
+            legifrance.gouv.fr
+          </FooterBodyItem>
+          <FooterBodyItem>
+            gouvernement.fr
+          </FooterBodyItem>
+          <FooterBodyItem>
+            service-public.fr
+          </FooterBodyItem>
+          <FooterBodyItem>
+            data.gouv.fr
+          </FooterBodyItem>
+        </FooterBody>
+        <FooterPartners data-testid="footerpartners">
+          <FooterPartnersTitle data-testid="title">Nos partenaires</FooterPartnersTitle>
+          <FooterPartnersSecondaryTitle data-testid="title">Et plus...</FooterPartnersSecondaryTitle>
+          <FooterPartnersLogo isMain href="/" imageSrc="img/logo1" imageAlt="Logo 1" data-testid="logo" />
+          <FooterPartnersLogo href="/" imageSrc="img/logo2" imageAlt="Logo 2" />
+          <FooterPartnersLogo href="/" imageSrc="img/logo3" imageAlt="Logo 3" />
+          <FooterPartnersLogo href="/" imageSrc="img/logo4" imageAlt="Logo 4" />
+        </FooterPartners>
+        <FooterBottom data-testid="bottom">
+          <FooterLink href="/" data-testid="link">Plan du site</FooterLink>
+          <FooterLink href="/">Accessibilité: non/partiellement/totalement conforme</FooterLink>
+          <FooterLink href="/">Mentions légales</FooterLink>
+          <FooterLink href="/">Données personnelles</FooterLink>
+          <FooterLink href="/">Gestion des cookies</FooterLink>
+          <FooterCopy href="/">© République Française 2020</FooterCopy>
+        </FooterBottom>
+      </Footer>,
+    );
+    const footer = screen.getByTestId('footer');
+    expect(footer).toMatchSnapshot();
+  });
 });
