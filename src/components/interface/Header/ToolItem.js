d1eb796ead4e9f8cc64bf709f07ff6d5e2b2704b
Desoindx <xavier.desoindre@hotmail.fr>
2023-02-11 12:03:37 +0100

    fix: always close mobile sidemenu on click

--- src/components/interface/Header/ToolItem.js
+++ src/components/interface/Header/ToolItem.js
@@ -22,8 +22,11 @@ const ToolItem = ({
   };
 
   return (
+    // eslint-disable-next-line max-len
+    // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions
     <li
       {...dataAttributes.getAll(remainingProps)}
+      onClick={() => onOpenNav(false)}
     >
       {as ? (
         <HtmlTag
