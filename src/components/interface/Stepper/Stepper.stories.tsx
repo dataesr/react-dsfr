a388b7a288b18dfda2a00783eced7074bbba00fc
mialy <rakoto.mk@gmail.com>
2023-05-10 18:27:10 +0200

    feat(Stepper):add stories

---
+++ src/components/interface/Stepper/Stepper.stories.tsx
@@ -0,0 +1,36 @@
+import { ComponentStory } from '@storybook/react';
+
+import Stepper from './Stepper';
+
+export default {
+  title: 'Composants/Stepper',
+  component: Stepper,
+  argTypes: {},
+  parameters: {
+    docs: {
+      description: {
+        component: "L'indicateur d'étapes permet à l'utilisateur de se repérer dans un formulaire.",
+      },
+    },
+  },
+};
+
+const Template: ComponentStory<typeof Stepper> = (args) => <Stepper {...args} />;
+
+export const FirstStep = Template.bind({});
+FirstStep.args = {
+  steps: 4,
+  currentStep: 1,
+  currentTitle: 'Titre de l\'étape en cours',
+  nextStepTitle: 'Titre de la prochaine étape',
+};
+FirstStep.storyName = "Indicateur d'étapes";
+
+export const LastStep = Template.bind({});
+LastStep.args = {
+  steps: 4,
+  currentStep: 4,
+  currentTitle: 'Titre de l\'étape en cours',
+  nextStepTitle: 'Titre de la prochaine étape',
+};
+LastStep.storyName = "Indicateur d'étapes";
