ae58a212d744cc558ed683942c025aa9abf51177
Anne L'Hôte <anne.lhote@gmail.com>
2026-03-09 12:01:20 +0100

    fix(linter): Add space before pipe

--- src/components/foundation/grid/Container.d.ts
+++ src/components/foundation/grid/Container.d.ts
@@ -2,16 +2,24 @@ import * as React from 'react';
 
 export type ContainerChildren = React.ReactNode[] | React.ReactNode | string;
 
-export type ContainerAs = 'article' | 'aside' | 'div' |'header' | 'footer' | 'main' | 'nav' | 'section';
+export type ContainerAs =
+  | 'article'
+  | 'aside'
+  | 'div'
+  | 'header'
+  | 'footer'
+  | 'main'
+  | 'nav'
+  | 'section';
 
 export interface ContainerProps {
   /**
-     * Container children node (should be Rows)
-     */
+   * Container children node (should be Rows)
+   */
   children?: ContainerChildren;
   /**
-     * Container with no outer margins
-     */
+   * Container with no outer margins
+   */
   fluid?: boolean;
   spacing?: string;
   role?: string;
